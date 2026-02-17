6786a590de1134acb2b804a088f4e5bdccbe820b
Jennifer Khang <jenniferkhang07@gmail.com>
2026-02-18 04:10:20 +0700

    feat: add base function and definition for cell and boardMatrix

--- src/java/boardMatrix.java
+++ src/java/boardMatrix.java
@@ -1,3 +1,9 @@
+/*
+    TO DO: 
+    1. Validation Input, was the input legal? Was there any spacing?
+    2. What if it was numbers instead of characters from A-Z?
+ */
+
 public class boardMatrix
 {
     public cell[][] mat;
@@ -5,7 +11,7 @@ public class boardMatrix
 
     public boardMatrix(int R, int C)
     {
-        this.nnRows = R;
+        this.nRows = R;
         this.nCols = C;
         this.mat = new cell[R][C];
     }
@@ -46,45 +52,102 @@ public class boardMatrix
         }
     }
 
-    public String tulisboardMatrix(boardMatrix boardMatrix) 
-    {
+    public String tulisBoardMatrix(boardMatrix boardMatrix) {
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < boardMatrix.nRows; i++) {
-            for (int j = 0; j < boardMatrix.nCols; j++)
-            {
-                cell val = boardMatrix.mat[i][j];
-                if (Math.abs(val - Math.round(val)) < 0.000000001)
-                {
-                    sb.append((int) val);
+            for (int j = 0; j < boardMatrix.nCols; j++) {
+
+                cell cell = boardMatrix.mat[i][j];
+
+                if (cell.hasQueen()) {
+                    sb.append("#");
+                } else {
+                    sb.append(cell.getColor());
                 }
-                else
-                {
-                    sb.append(String.format("%.4f", val).replaceAll("0+$", "").replaceAll("\\.$", ""));
+
+                if (j < boardMatrix.nCols - 1) {
+                    sb.append(" ");
                 }
-                if (j < boardMatrix.nCols - 1) sb.append(" ");
             }
             sb.append("\n");
         }
+
         return sb.toString();
     }
 
-    // FIX
-    public static boardMatrix textToboardMatrix(String input) //Mengubah input String menjadi boardMatrix
-    {
+    public boolean isSquare(boardMatrix mat){
+        boolean square = false;
+        if (mat.nRows == mat.nCols){square = true;}
+        return square;
+    }
+
+    public static String[] splitRow(String line) {
+        line = line.trim();
+
+        if (line.contains(" ")) {
+            return line.split("\\s+");
+        }
+
+        return line.split("");
+    }
+    
+    public boolean validateLetters(String[] lines, int expectedCols){
+        boolean valid = true;
+        for (int i = 0; i < lines.length; i++) {
+
+            String[] tokens = splitRow(lines[i]);
+
+            if (tokens.length != expectedCols){
+                System.out.print("There are some columns that is different.");
+                valid = false;
+            }
+
+            for (int j = 0; j < tokens.length; j++) {
+
+                if (tokens[j].length() != 1) {
+                    System.out.print("Invalid token length.");
+                    valid = false;
+                }
+
+                char c = tokens[j].charAt(0);
+
+                if (!Character.isLetter(c)) {
+                    System.out.print("Invalid character at row" + i + " col" + j + ".");
+                    valid = false;
+                }
+            }
+        }
+        return valid;
+    }
+
+    public static boardMatrix textToBoardMatrix(String input){ //Mengubah input String menjadi boardMatrix
         if (input == null || input.isBlank()) return null;
         String[] lines = input.trim().split("\\r?\\n+");
-        int totalnRows = lines.length;
+        int nRows = lines.length;
 
         // Jika spasi kosong akan dihilangkan
-        for (int i = 0; i < totalnRows; i++) lines[i] = lines[i].trim();
+        for (int i = 0; i < nRows; i++) lines[i] = lines[i].trim();
         if (lines[0].isEmpty()) return null;
 
         // Deteksi kolom
-        String[] firstLine = lines[0].split("\\s+");
+        String[] firstLine = splitRow(lines[0]);
         int nCols = firstLine.length;
+        
+        // Buat matrix kosong
+        boardMatrix board = new boardMatrix(nRows, nCols);
+        for (int i = 0; i < nRows; i++) {
+            String[] tokens = splitRow(lines[i]);
 
-        // Mengambil derajat regresi/interpolasi
-        String[] lastTokens = lines[totalnRows - 1].split("\\s+");
-        boolean lastIsDegree= (lastTokens.length == 1 && lastTokens[0].matches("-?\\d+(\\.\\d+)?"));
+            for (int j = 0; j < nCols; j++) {
+                char color = tokens[j].charAt(0);  
+
+                board.mat[i][j] = new cell(i, j, color);
+            }
+        }
+
+        return board;
+
     }
+    
 }
